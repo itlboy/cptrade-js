68867dc9fed57524ed31d0131a3e5ffa33b15924
Kien <superman@earth.com>
2021-01-02 04:10:45 +0700

    update

--- modules/BaseTraderAccount.js
+++ modules/BaseTraderAccount.js
@@ -1,16 +1,19 @@
 var fs = require('fs');
 var fetch = require("node-fetch");
+var _ = require("lodash");
+var dayjs = require("dayjs");
 class BaseTraderAccounts {
     baseUrl = "https://busstrade.com/api/";
     referrerUrl = "https://busstrade.com/dashboard";
+    expertSessionTime = 3600;
+    historyDelayTime = 5;
     options = {};
-
     constructor(options) {
         this.options = options;
     }
 
     loadCookies() {
-        return JSON.parse(fs.readFileSync("assets/busstrade/nkien.bk@gmail.com.json"));
+        return JSON.parse(fs.readFileSync("/data/app/assets/bussTrade-nkien.bk@gmail.com.json"));
     }
 
     async getToken() {
@@ -30,8 +33,6 @@ class BaseTraderAccounts {
             }
             rawCookies += name + "=" + value + ";";
         });
-
-        console.log(rawCookies);
         return rawCookies;
     }
     async request(method, path, data) {
@@ -74,25 +75,97 @@ class BaseTraderAccounts {
     }
 
     async getDemoHistory() {
-
+        return await this.apiGet("histories/demo?page=1");
     }
 
     async getExpertHistory() {
-
+        return await this.apiGet("histories/copytrade?page=1");
     }
 
-    async getExpertPendingOrder(callback, demo = false) {
-        var result;
-        if (demo) {
-            result = await this.getDemoHistory();
+    async getExpertPendingOrder(demo = false) {
+        return new Promise(async (resolve, reject) => {
+            var historyResult;
+            if (demo) {
+                historyResult = await this.getDemoHistory();
+            } else {
+                historyResult = await this.getExpertHistory();
+            }
+            var orders = historyResult.data.orders.data;
+            var pendingOrder = this.getPendingOrderFromHistory(orders);
+            resolve(pendingOrder);
+        })
+    }
+
+    getNumberOrderOfCurrentSession(orders) {
+        return this.filterCurrentSessionOrders(orders).length;
+    }
+
+    filterCurrentSessionOrders(orders) {
+        var results = [];
+        var currentTime = dayjs().unix();
+        _.forEach(orders, (order) => {
+            var orderTimeString = order.created_at;
+            var orderTimeStamp = dayjs(orderTimeString).unix();
+            if ((currentTime - orderTimeStamp) < this.expertSessionTime) {
+                results.push(order);
+            }
+        })
+        return results;
+    }
+
+    listenPendingOrder(callback, demo = false) {
+        var isCheckingHistory;
+        setInterval(async() => {
+            var currentSecond = dayjs().format("s");
+            if (!isCheckingHistory && currentSecond > 21 && currentSecond < 30) {
+                isCheckingHistory = true;
+                console.time("getExpertPendingOrder");
+                var pendingOrder = await this.getExpertPendingOrder(demo);
+                console.timeEnd("getExpertPendingOrder");
+                if (pendingOrder != null) {
+                    callback(pendingOrder);
+                    setTimeout(() => {
+                        isCheckingHistory = false;
+                    }, 15000)
+                } else {
+                    isCheckingHistory = false;
+                }
+            }
+        }, 1000)
+    }
+
+    isEnableTobet(order) {
+        var orderTimeString = order.created_at;
+        var orderTimeStamp = dayjs(orderTimeString).unix();
+        var currentTime = dayjs().unix();
+        console.log("delta time", currentTime - orderTimeStamp, (currentTime - this.historyDelayTime) / 30, orderTimeStamp / 30);
+        if (parseInt((currentTime - this.historyDelayTime) / 30) == parseInt(orderTimeStamp / 30)) {
+            return true;
         } else {
-            result = await this.getExpertHistory();
-            callback();
-    }
+            return false;
+        }
     }
 
-    filterPendingOrder() {
-        
+    getPendingOrderFromHistory(orders) {
+        var orderNumber = this.getNumberOrderOfCurrentSession(orders);
+        var lastestOder = orders[0];
+        if (lastestOder.status != 0) {
+            return null;
+        } else {
+            return {
+                market: lastestOder.market_name,
+                action: lastestOder.action,
+                amount: lastestOder.amount,
+                orderNumber: orderNumber,
+                enableToBet: this.isEnableTobet(lastestOder)
+            }
+        }
+    }
+
+    async followExpert(callback, demo = false) {
+        this.getExpertPendingOrder({
+
+        }, demo)
     }
 
 }
--- test.js
+++ test.js
@@ -1,11 +1,13 @@
 BaseTraderAccount = require("./modules/BaseTraderAccount")
 var test = new BaseTraderAccount();
 
-
 var run = async  function () {
-    var result = await test.apiGet("profile/user");
-    console.log("heheh");
-    console.log(result);
+//    var result = await test.apiGet("profile/user");
+//    var result  = await test.getExpertPendingOrder(true);
+//    console.log(result);
+    test.listenPendingOrder((order) => {
+        console.log(order);
+    }, true)
 }
 
 run();
